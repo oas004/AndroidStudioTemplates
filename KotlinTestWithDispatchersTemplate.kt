5607008d788ff6465e487332ee4b06a9a66f164f
Odin Asbjørnsen <54936943+oas004@users.noreply.github.com>
2022-02-28 13:09:43 +0100

    Add test with dispatchers
    Adding simple test with imports and set coroutine test dispatchers

---
+++ KotlinTestWithDispatchersTemplate.kt
@@ -0,0 +1,32 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+
+#end
+#parse("File Header.java")
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ${NAME} {
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @After
+    fun teardown() {
+        Dispatchers.resetMain()
+    }
+    
+    @Test
+    fun `test`() = runTest {
+    }
+
+}
